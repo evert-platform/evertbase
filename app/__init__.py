71e94f20a20178f800a341cd3683daf2964dfdb5
NeillHerbst <n.herbst6@gmail.com>
2017-01-31 15:11:39 +0200

    moved pluginmanager setup to create_app function from manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_plugins import PluginManager
 
 bootstrap = Bootstrap()
 
@@ -9,7 +10,12 @@ def create_app():
     UPLOADFOLDER = 'static/uploads/'
     app.config['SECRET_KEY'] = 'hard to guess string'
     bootstrap.init_app(app)
+
     app.config['UPLOAD_FOLDER'] = UPLOADFOLDER
+
+    # Configuration of flask_plugins extension
+    pluginmanager = PluginManager(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
