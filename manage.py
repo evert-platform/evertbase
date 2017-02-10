91ea639e477bb5b6bb63599b6b9881c317408633
Neill Herbst <n.herbst6@gmail.com>
2017-02-10 14:52:23 +0200

    added save destination for plugins

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from evert.app import create_app
 from flask import current_app
 from flask_plugins import Plugin
+from flask_uploads import UploadSet, DATA, configure_uploads, ALL
 
 
 class AppPlugin(Plugin):
@@ -11,6 +12,14 @@ class AppPlugin(Plugin):
 # Creating app
 app = create_app()
 app.config.from_pyfile('config.py')
+
+# File upload configuration for data files
+data = UploadSet('file', DATA)
+configure_uploads(app, data)
+
+# File upload configuration for ZIP files
+plugin_upload = UploadSet('plugin', ALL)
+configure_uploads(app, plugin_upload)
 
 if __name__ == '__main__':
     app.run()
