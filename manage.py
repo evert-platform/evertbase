67a25ed89ca332c206180879062a3d4e1d665e5b
Neill Herbst <n.herbst6@gmail.com>
2017-02-10 13:20:57 +0200

    moved app configuration to separate .py file

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 from evert.app import create_app
 from flask import current_app
 from flask_plugins import Plugin
-from flask_uploads import UploadSet, DATA, configure_uploads
-import os
 
 
 class AppPlugin(Plugin):
@@ -12,17 +10,7 @@ class AppPlugin(Plugin):
 
 # Creating app
 app = create_app()
-
-# Basic directory paths
-app.config['BASE_DIR'] = os.path.dirname(__file__)
-app.config['PLUGIN_DIR'] = os.path.join(app.config['BASE_DIR'], 'app/plugins')
-app.config['STATIC_DIR'] = os.path.join(app.config['BASE_DIR'], 'app/static')
-
-# Configuration of flask_uploads extension
-data = UploadSet('file', DATA)
-app.config['UPLOADED_FILE_DEST'] = os.path.join(app.config['STATIC_DIR'], 'uploads')
-configure_uploads(app, data)
-
+app.config.from_pyfile('config.py')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
