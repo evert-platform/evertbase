1fbaf322dd8ce1cbb23fb2874c9e0516dfd408fb
NeillHerbst <n.herbst6@gmail.com>
2017-02-14 14:26:00 +0200

    fixed import error added extra tests

--- app/testing/tests.py
+++ app/testing/tests.py
@@ -1,6 +1,6 @@
 from flask_testing import TestCase
 import unittest
-from evert.app import create_app
+from app import create_app
 
 
 
@@ -13,22 +13,32 @@ class TestNotRenderTemplates(TestCase):
 
     render_templates = False
 
-    def test_assert_not_process_the_index_template(self):
+    def test_assert_route_index_template(self):
         response = self.client.get("/")
 
         self.assert_template_used('index.html')
 
 
-    def test_assert_not_process_the_plot_template(self):
+    def test_assert_route_plot_template(self):
         response = self.client.get("/plotlyplotting")
 
         self.assert_template_used('plot.html')
 
 
-    def test_assert_not_process_the_dataviewer_template(self):
+    def test_assert_route_dataviewer_template(self):
         response = self.client.get("/dataviewer")
 
         self.assert_template_used('dataviewer.html')
+
+    def test_assert_route_uploads_template(self):
+        response = self.client.get("/upload")
+
+        self.assert_template_used('uploads.html')
+
+    def test_assert_route_plugins_template(self):
+        response = self.client.get("/plugins")
+
+        self.assert_template_used('plugins.html')
 
 
 
