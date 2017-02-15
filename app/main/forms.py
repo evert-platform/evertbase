b11a14e71efecc0cf6d7a0d90e764004d4f19072
NeillHerbst <n.herbst6@gmail.com>
2017-02-15 17:39:24 +0200

    fixed select choices

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,7 +12,7 @@ class DataSelectForm(FlaskForm):
     select = SelectField('Please select file to plot.', choices='', id='text')
     selectX = SelectField('', choices='', id='plotX')
     selectY = SelectField('', choices='', id='plotY')
-    selectType = SelectField('', choices=[('scatter', ''), ('Line', '')], id='plotType')
+    selectType = SelectField('', choices=[('Scatter', 'Scatter'), ('Line', 'Line')], id='plotType')
 
 
 class PluginsForm(FlaskForm):
