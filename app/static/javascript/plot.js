af41318e7e36a464cada53676a5fd5f76c604980
Neill Herbst <n.herbst6@gmail.com>
2017-02-16 14:58:23 +0200

    clearing of datasets

--- app/static/javascript/plot.js
+++ app/static/javascript/plot.js
@@ -16,21 +16,68 @@ $(document).ready(function () {
 });
 
 $(function() {
-			  $('input#Submit').on('click', function() {
-				$.getJSON('/_plotdata', {
-				  plotdata: $('select[name="select"]').val(),
-                    xaxis: $('select#plotxaxis').val(),
-                    yaxis: $('select#plotyaxis').val(),
-                    type: $('select#plotType').val()
-				}, function(data) {
-				    var $plotarea = $('#plotarea');
-				    $plotarea.empty();
-				    $plotarea.append('<hr><br>');
-				    $plotarea.append(data.plot);
-				});
-				return false;
-			  });
-			});
+    var xset = '';
+    var yset = '';
+    var sets = [];
+    var $datasets = $('select#datasets');
+    var $plotxaxis = $('select#plotxaxis');
+    var $plotyaxis = $('select#plotyaxis');
+
+
+    $('input#setdatabtn').on('click', function () {
+        var multiplot = document.getElementById('multiplot').checked;
+        var xaxis = $plotxaxis.val();
+        var yaxis = $plotyaxis.val();
+
+        $('button#cleardataset').attr('disabled', false);
+        $('button#deletedataset').attr('disabled', false);
+
+        if (multiplot == true){
+            xset += (xaxis +' ');
+            yset += (yaxis + ' ');
+            sets.push([xaxis, yaxis]);
+        } else {
+            xset = $plotxaxis.val();
+            yset = $plotyaxis.val();
+        }
+
+        var optionsAsString = "";
+
+
+        for(var i = 0; i < sets.length; i++) {
+            optionsAsString += "<option value='" + sets[i] + "'>" + sets[i] + "</option>";
+        }
+        $datasets.empty().append(optionsAsString).attr('disabled', false);
+
+    });
+
+    $('button#cleardataset').on('click', function () {
+        $datasets.empty().append("<option></option>").attr('disabled', true);
+        $(this).attr('disabled', true);
+        $('button#deletedataset').attr('disabled', true);
+        xset = '';
+        yset = '';
+        sets = [];
+    });
+
+
+    $('input#Submit').on('click', function() {
+        $.getJSON('/_plotdata', {
+          plotdata: $('select[name="select"]').val(),
+            xaxis: $('select#plotxaxis').val(),
+            yaxis: $('select#plotyaxis').val(),
+            type: $('select#plotType').val(),
+           xset: xset,
+            yset: yset
+        }, function(data) {
+            var $plotarea = $('#plotarea');
+            $plotarea.empty();
+            $plotarea.append('<hr><br>');
+            $plotarea.append(data.plot);
+        });
+        return false;
+        });
+    });
 
 $(function () {
     $('select#plotfile').on('change', function(){
@@ -48,6 +95,12 @@ $(function () {
             })
         })
     });
+
+$(function () {
+
+});
+
+
 
 $(function () {
     $('button#deleteplot').on('click', function(){
