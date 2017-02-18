1928083fbebf255ce2ec4fe8a3a20806cf814a15
NeillHerbst <n.herbst6@gmail.com>
2017-02-18 15:38:14 +0200

    recoded project tests using pytest

--- app/testing/tests.py
+++ app/testing/tests.py
@@ -1,46 +1,33 @@
-from flask_testing import TestCase
-import unittest
 from app import create_app
+import pytest
+from flask import url_for
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+@pytest.mark.usefixtures('client_class')
+class TestViews:
+
+    def test_index(self):
+        assert self.client.get(url_for('main.index')).status_code == 200
+
+    def test_plot(self):
+        assert self.client.get(url_for('main.plot')).status_code == 200
+
+    def test_plugins(self):
+        assert self.client.get(url_for('main.plugins')).status_code == 200
+
+    def test_uploads(self):
+        assert self.client.get(url_for('main.upload')).status_code == 200
+
+    def test_dataviewer(self):
+        assert self.client.get(url_for('main.dataview')).status_code == 200
 
 
 
-class TestNotRenderTemplates(TestCase):
-
-    def create_app(self):
-        app = create_app()
-        app.config['TESTING'] = True
-        return app
-
-    render_templates = False
-
-    def test_assert_route_index_template(self):
-        response = self.client.get("/")
-
-        self.assert_template_used('index.html')
-
-
-    def test_assert_route_plot_template(self):
-        response = self.client.get("/plotlyplotting")
-
-        self.assert_template_used('plot.html')
-
-
-    def test_assert_route_dataviewer_template(self):
-        response = self.client.get("/dataviewer")
-
-        self.assert_template_used('dataviewer.html')
-
-    def test_assert_route_uploads_template(self):
-        response = self.client.get("/upload")
-
-        self.assert_template_used('uploads.html')
-
-    def test_assert_route_plugins_template(self):
-        response = self.client.get("/plugins")
-
-        self.assert_template_used('plugins.html')
 
 
 
-if __name__ == '__main__':
-    unittest.main()
