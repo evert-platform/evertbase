678177a198f90de423360c8968715883aa0c3458
Neill Herbst <n.herbst6@gmail.com>
2017-02-20 09:14:32 +0200

    removed old config setup method

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = True
-SECRET_KEY = 'hard to guess string'
-BASE_DIR = os.path.dirname(__file__)
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-UPLOADED_FILE_DEST = os.path.join(STATIC_DIR, 'uploads')
-UPLOADED_PLUGIN_DEST = os.path.join(BASE_DIR, 'plugins')
-USER_DOCUMENTS = os.path.expanduser('~/Documents')
