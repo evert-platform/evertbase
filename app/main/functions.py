ea33aff5ce1852de37b42a7a20de711a08574445
Neill Herbst <n.herbst6@gmail.com>
2017-02-22 12:01:36 +0200

    removed manual date parser

--- app/main/functions.py
+++ app/main/functions.py
@@ -94,37 +94,6 @@ def unique_headers(table_key, initial=False):
     return fieldnames
 
 
-def date_parser(start_date, end_date):
-    """
-    This is a date parsing function to automatically determine the best date format to you for time-series data
-    :param start_date: A datetime object of the start date
-    :param end_date: A datetime object of the end date
-    :return: Two DateFormatter objects of the major and minor date format respectively
-    """
-
-    delta = end_date - start_date
-    days = delta.days
-    seconds = delta.seconds
-
-    if days > 365:
-        major_fmt = '%Y'
-        minor_fmt = '%m-%d'
-
-    elif 1 < days < 365:
-        major_fmt = '%Y-%m-%d'
-        minor_fmt = '%Y-%m-%d %H:00'
-
-    elif days == 0 and seconds > 3600:
-        major_fmt = '%H:%M'
-        minor_fmt = '%H:%M:%S'
-
-    elif days == 0 and seconds < 3600:
-        major_fmt = '%M:00'
-        minor_fmt = '%M:%S'
-
-    return mdates.DateFormatter(major_fmt), mdates.DateFormatter(minor_fmt)
-
-
 def copy_files(src, dst, check_mod_time=False):
     """
     Copies subdirectories from one directory to another
