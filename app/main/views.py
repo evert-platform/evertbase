9b366176e8f381c6b5f693780e92e766dbcef8c8
NeillHerbst <n.herbst6@gmail.com>
2017-02-23 15:23:44 +0200

    moved async views to own file

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,8 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from flask import render_template, flash, request, jsonify, current_app
 from . import main
 from .forms import FileUploadForm, DataViewerForm, DataSelectForm, PluginsUploadForm, PluginsForm
-from flask_plugins import PluginManager, get_plugin_from_all
 from . import functions as funcs
-from zipfile import ZipFile, BadZipFile
-import mpld3
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -48,46 +44,6 @@ def plot():
     return render_template('plot.html', form=form)
 
 
-# Async view
-@main.route('/_plotdata', methods=['GET'])
-def _plotdata():
-    fig, ax = plt.subplots()
-    filepath = request.args.get('plotdata', 0, type=str)
-
-    hdf5store = current_app.config["HDF5_STORE"]
-    store = pd.HDFStore(hdf5store)
-    data = store.get(filepath)
-    store.close()
-
-    plottype = request.args.get('type', 0, type=str)
-    xset = request.args.getlist('xset[]')
-    yset = request.args.getlist('yset[]')
-
-    for x, y in zip(xset, yset):
-        if data[x].dtype == 'O':
-            data[x] = pd.to_datetime(data[x])
-
-        if plottype == 'Line':
-            data.plot.line(x=x, y=y, ax=ax)
-
-        elif plottype == 'Scatter':
-            data.plot.line(x=x, y=y, lw=0, marker='.', ax=ax)
-
-    ax.legend(loc=0)
-    fig.tight_layout()
-    div = mpld3.fig_to_dict(fig)
-    return jsonify(plot=div)
-
-
-@main.route('/_plotdetails', methods=['GET'])
-def _plotdetails():
-
-    table_key = request.args.get('plotfile', 0, type=str)
-    headers = funcs.unique_headers(table_key)
-
-    return jsonify(success=True, headers=headers)
-
-
 @main.route('/plugins', methods=['GET', 'POST'])
 def plugins():
     form = PluginsForm()
@@ -95,42 +51,6 @@ def plugins():
     form.select_disabled.choices = funcs.checkplugins(enabled=False)
     form.select_enabled.choices = funcs.checkplugins(enabled=True)
     return render_template('plugins.html', form=form, form2=form2)
-
-
-# Async view
-@main.route('/_enable_plugin', methods=['GET', 'POST'])
-def _enable_plugins():
-    plugin = request.args.get('enableplugins', 0, type=str)
-    pluginsmanager = PluginManager()
-    pluginsmanager.enable_plugins([get_plugin_from_all(plugin)])
-    return jsonify(sucess=True)
-
-
-# Async view
-@main.route('/_disable_plugin', methods=['GET', 'POST'])
-def _disable_plugins():
-    plugin = request.args.get('disableplugins', 0, type=str)
-    pluginsmanager = PluginManager()
-    pluginsmanager.disable_plugins([get_plugin_from_all(plugin)])
-    return jsonify(success=True)
-
-
-# Async view
-@main.route('/_uploadp', methods=['GET', 'POST'])
-def _upload_plugins():
-    if request.method == 'POST':
-        zip_file = request.files['file']
-        try:
-            zipfile = ZipFile(zip_file)
-            zipfile.extractall(current_app.config['UPLOADED_PLUGIN_DEST'])
-            success = True
-            msg = 'Success: Plugin uploaded successfully'
-
-        except BadZipFile:
-            success = False
-            msg = 'Error: Ensure file is a zip file'
-
-    return jsonify(success=success, msg=msg)
 
 
 @main.route('/dataviewer', methods=['GET', 'POST'])
