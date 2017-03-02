fc17532bdf23a1446d620b3dfafb59e4dd6d81aa
NeillHerbst <n.herbst6@gmail.com>
2017-03-02 17:20:24 +0200

    js for open tab

--- app/static/javascript/uploads.js
+++ app/static/javascript/uploads.js
@@ -1,23 +1,40 @@
 $(document).ready(function () {
     var $uploadtab = $('li#upload');
     var $plantsetuptab = $('li#setup');
+    var $opentab = $('li#open');
 
 
     $uploadtab.on('click', function () {
         $(this).addClass('active');
         $plantsetuptab.removeClass('active');
+        $opentab.removeClass('active');
 
-        $('div#upload_file').show();
-        $('div#plantsetup').hide();
+        $('fieldset#open_file').hide();
+        $('fieldset#upload_file').show();
+        $('fieldset#plantsetup').hide();
+
 
     });
 
     $plantsetuptab.on('click', function () {
         $(this).addClass('active');
         $uploadtab.removeClass('active');
+        $opentab.removeClass('active');
 
-        $('div#upload_file').hide();
-        $('div#plantsetup').show();
+        $('fieldset#open_file').hide();
+        $('fieldset#upload_file').hide();
+        $('fieldset#plantsetup').show();
+    });
+
+    $opentab.on('click', function () {
+        $(this).addClass('active');
+
+        $uploadtab.removeClass('active');
+        $plantsetuptab.removeClass('active');
+
+        $('fieldset#open_file').show();
+        $('fieldset#upload_file').hide();
+        $('fieldset#plantsetup').hide();
     })
 
 });
