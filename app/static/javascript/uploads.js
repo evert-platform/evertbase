55f85d87d52595332bd84acccb01bef5c0cf2c57
NeillHerbst <n.herbst6@gmail.com>
2017-03-04 20:11:56 +0200

    ajax data uploads

--- app/static/javascript/uploads.js
+++ app/static/javascript/uploads.js
@@ -3,15 +3,28 @@ $(document).ready(function () {
     var $plantsetuptab = $('li#setup');
     var $opentab = $('li#open');
 
+    $opentab.on('click', function () {
+        $(this).addClass('active');
+
+        $uploadtab.removeClass('active');
+        $plantsetuptab.removeClass('active');
+
+        $('fieldset#datafileform').show();
+        $('input#open_file').show();
+        $('input#upload_file').hide();
+        $('fieldset#plantsetup').hide();
+
+    });
 
     $uploadtab.on('click', function () {
         $(this).addClass('active');
         $plantsetuptab.removeClass('active');
         $opentab.removeClass('active');
 
-        $('fieldset#open_file').hide();
-        $('fieldset#upload_file').show();
+        $('input#open_file').hide();
+        $('input#upload_file').show();
         $('fieldset#plantsetup').hide();
+        $('fieldset#datafileform').show();
 
 
     });
@@ -21,20 +34,61 @@ $(document).ready(function () {
         $uploadtab.removeClass('active');
         $opentab.removeClass('active');
 
-        $('fieldset#open_file').hide();
-        $('fieldset#upload_file').hide();
+        $('input#open_file').hide();
+        $('input#upload_file').hide();
         $('fieldset#plantsetup').show();
+        $('fieldset#datafileform').hide();
     });
 
-    $opentab.on('click', function () {
-        $(this).addClass('active');
 
-        $uploadtab.removeClass('active');
-        $plantsetuptab.removeClass('active');
-
-        $('fieldset#open_file').show();
-        $('fieldset#upload_file').hide();
-        $('fieldset#plantsetup').hide();
-    })
 
 });
+
+
+$(function() {
+
+
+        var $openbtn = $('input#open_file');
+        var $uploadbtn = $('input#upload_file');
+
+      $openbtn.on('click', function() {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var formdata = new FormData($('#datafile')[0]);
+
+
+        $.ajax({
+            url: '/_dataopen',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: formdata,
+            dataType: 'json',
+            success: function(data) {
+                }
+            });
+        });
+
+      $uploadbtn.on('click', function () {
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        var formdata = new FormData($('#datafile')[0]);
+
+
+        $.ajax({
+            url: '/_dataupload',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: formdata,
+            dataType: 'json',
+            success: function(data) {
+            }
+            });
+
+      })
+
+      });
