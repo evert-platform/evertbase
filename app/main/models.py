e892eaf598701deeaa1f81e47e5bdda44f456416
NeillHerbst <n.herbst6@gmail.com>
2017-03-04 20:33:04 +0200

    added default behavior for handling new data

--- app/main/models.py
+++ app/main/models.py
@@ -2,6 +2,7 @@ import sqlite3 as sql
 import pandas as pd
 import numpy as np
 import os
+from datetime import datetime
 
 DATABASE = os.path.abspath('./app/static/uploads/EvertStore.db')
 
@@ -13,7 +14,10 @@ def create_db(name):
         # Create database tables
         cur.executescript('''CREATE TABLE IF NOT EXISTS plants
                               (plant_id INTEGER PRIMARY KEY  AUTOINCREMENT,
-                               plant_name TEXT );
+                               plant_name TEXT,
+                               opened INTEGER,
+                               uploaded INTEGER,
+                               timestamp TEXT);
 
                             CREATE TABLE  IF NOT EXISTS sections
                             (section_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -50,13 +54,29 @@ def create_db(name):
         con.commit()
 
 
-def write_data_to_db(file_name, plant_name):
+def write_data_to_db(file_name, plant_name, open, upload):
     with sql.connect(DATABASE) as con:
         # creating database cursor object
         cur = con.cursor()
 
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
         # adding plant name
-        cur.execute("""INSERT INTO plants (plant_name) VALUES (?)""", (plant_name,))
+        cur.execute("""INSERT INTO plants (plant_name, opened, uploaded, timestamp) VALUES (?, ?, ?, ?)""",
+                    (plant_name, open, upload, time))
+
+        # Finding default plant id
+        plant_id = cur.execute("""SELECT plant_id FROM plants
+                        WHERE plant_name = (?)""", (plant_name,)).fetchone()[0]
+
+        # adding defualt unit name
+        default_section_name = plant_name + '_Unit01'
+        cur.execute("""INSERT INTO sections (section_name, plant_id)
+                       VALUES (?, ?)""", (default_section_name, plant_id))
+
+        # getting unique id for default section
+        section_id = cur.execute("""SELECT section_id FROM sections
+                                    WHERE section_name = (?)""", (default_section_name,)).fetchone()[0]
 
         # handling csv file
         df = pd.read_csv(file_name)
@@ -68,10 +88,10 @@ def write_data_to_db(file_name, plant_name):
         cur_tags = cur.execute('SELECT tag_name FROM tags').fetchall()
 
         # initialize tag list to add to database
-        tags_commit = [(tag,) for tag in df_tags if tag not in cur_tags]
+        tags_commit = [(section_id, tag,) for tag in df_tags if tag not in cur_tags]
 
         # adding new tags to data base
-        cur.executemany('INSERT INTO tags (tag_name) VALUES (?)', tags_commit)
+        cur.executemany('INSERT INTO tags (section_id, tag_name) VALUES (?, ?)', tags_commit)
 
         # creating tagmap from tags in tags table
         tag_map = dict(cur.execute('SELECT tag_name, tag_id FROM tags').fetchall())
