76ad110ea8ea7931125ec86ba4c48a77f2d96151
NeillHerbst <n.herbst6@gmail.com>
2017-03-06 18:14:32 +0200

    all queries now use sqlalchemy

--- app/main/models.py
+++ app/main/models.py
@@ -1,137 +1,113 @@
-import sqlite3 as sql
 import pandas as pd
 import numpy as np
 import os
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-DATABASE = os.path.abspath('./app/static/uploads/EvertStore.db')
+db = SQLAlchemy()
 
+class BaseMixin:
 
-def create_db(name):
-    with sql.connect(name) as con:
-        cur = con.cursor()
-
-        # Create database tables
-        cur.executescript('''CREATE TABLE IF NOT EXISTS plants
-                              (plant_id INTEGER PRIMARY KEY  AUTOINCREMENT,
-                               plant_name TEXT UNIQUE,
-                               opened INTEGER,
-                               uploaded INTEGER,
-                               timestamp TEXT);
-
-                            CREATE TABLE  IF NOT EXISTS sections
-                            (section_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                             section_name TEXT,
-                             plant_id INTEGER,
-                             FOREIGN KEY(plant_id) REFERENCES plants(plant_id) );
-
-                             CREATE TABLE IF NOT EXISTS equipment
-                             (equipment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                              equipment_name TEXT,
-                              plant_id,
-                              section_id INTEGER,
-                              FOREIGN KEY(section_id) REFERENCES sections(section_id),
-                              FOREIGN KEY (plant_id) REFERENCES plants(plant_id));
-
-                            CREATE TABLE IF NOT EXISTS tags
-                            (tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                             section_id INTEGER,
-                             equipment_id INTEGER,
-                             tag_name TEXT NOT NULL,
-                             upper_bound NUMERIC,
-                             lower_bound NUMERIC,
-                             units TEXT,
-                             FOREIGN KEY (section_id) REFERENCES sections(section_id),
-                             FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id));
-
-                            CREATE TABLE IF NOT EXISTS measurement_data
-                            (timestamp,
-                             tag_id INTEGER,
-                             tag_value NUMERIC,
-                             FOREIGN KEY (tag_id) REFERENCES tags(tag_id));
-                            ''')
-
-        con.commit()
+    @classmethod
+    def create(cls, **kwargs):
+        obj = cls(**kwargs)
+        db.session.add(obj)
+        db.session.commit()
 
 
-def write_data_to_db(file_name, plant_name, open, upload, append=False):
-    with sql.connect(DATABASE) as con:
-        # creating database cursor object
-        cur = con.cursor()
-
-        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-
-        # adding plant name
-        cur.execute("""INSERT INTO plants (plant_name, opened, uploaded, timestamp) VALUES (?, ?, ?, ?)""",
-                    (plant_name, open, upload, time))
-
-        # Finding default plant id
-        plant_id = cur.execute("""SELECT plant_id FROM plants
-                        WHERE plant_name = (?)""", (plant_name,)).fetchone()[0]
-
-        # adding defualt unit name
-        default_section_name = plant_name + '_Unit01'
-        cur.execute("""INSERT INTO sections (section_name, plant_id)
-                       VALUES (?, ?)""", (default_section_name, plant_id))
-
-        # getting unique id for default section
-        section_id = cur.execute("""SELECT section_id FROM sections
-                                    WHERE section_name = (?)""", (default_section_name,)).fetchone()[0]
-
-        # handling csv file
-        df = pd.read_csv(file_name)
-        df = pd.melt(df, id_vars=df.columns.values[0])
-        df.columns = ['time_stamp', 'tag_id', 'tag_value']
-        df_tags = np.unique(df['tag_id'].values)
-
-        # current tags in the database
-        if append:
-            cur_tags = cur.execute('SELECT tag_name FROM tags').fetchall()
-
-            # initialize tag list to add to database
-            tags_commit = [(section_id, tag,) for tag in df_tags if tag not in cur_tags]
-
-        else:
-            # initialize tag list to add to database
-            tags_commit = [(section_id, tag,) for tag in df_tags]
-
-        # adding new tags to data base
-        cur.executemany('INSERT INTO tags (section_id, tag_name) VALUES (?, ?)', tags_commit)
-
-        # creating tagmap from tags in tags table
-        tag_map = dict(cur.execute('SELECT tag_name, tag_id FROM tags').fetchall())
-
-        # normalizing dataframe tag_id with tag_ids from tags table
-        df.tag_id = [tag_map[key] for key in df['tag_id'].values]
-
-        # adding data to tag_data table
-        df.to_sql('tag_data', con, if_exists='append', index=False)
-
-def get_tag_names():
-
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        tag_names = np.asarray(cur.execute('''SELECT tag_name FROM tags''').fetchall()).T[0]
-        tag_names = [(tag, tag) for tag in tag_names]
-
-    return tag_names
-
-def get_tag_data(tag_name):
-
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = cur.execute("""SELECT time_stamp, tag_value
-                              FROM tag_data
-                              JOIN tags
-                              ON tag_data.tag_id=tags.tag_id
-                              WHERE tag_name=(?)""", (tag_name,)).fetchall()
-    return data
 
 
-def get_plant_names():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = cur.execute("""SELECT plant_name
-                              FROM plants""").fetchall()
+class Plants(BaseMixin, db.Model):
+    plant_id = db.Column('plant_id', db.Integer, primary_key=True)
+    plant_name = db.Column('plant_name', db.VARCHAR(50), unique=True)
+    opened = db.Column('opened', db.Integer)
+    uploaded = db.Column('uploaded', db.Integer)
+    time = db.Column('time', db.TEXT)
 
-    return data
+
+class Sections(BaseMixin, db.Model):
+    section_id = db.Column('section_id', db.Integer, primary_key=True)
+    section_name = db.Column('section_name', db.TEXT)
+    plant_id = db.Column('plant_id', db.Integer, db.ForeignKey('plants.plant_id', ondelete='CASCADE',
+                                                               onupdate='CASCADE'))
+
+
+class Equipment(BaseMixin, db.Model):
+    equipment_id = db.Column('equipment_id', db.Integer, primary_key=True)
+    equipment_name = db.Column('equipment_name', db.Text)
+    plant_id = db.Column('plant_id', db.ForeignKey('plants.plant_id', onupdate='CASCADE', ondelete="CASCADE"))
+    section_id = db.Column('section_id', db.ForeignKey('sections.section_id', onupdate='CASCADE', ondelete="CASCADE"))
+
+
+class Tags(BaseMixin, db.Model):
+    tag_id = db.Column('tag_id', db.Integer, primary_key=True)
+    section_id = db.Column('section_id', db.ForeignKey('sections.section_id', ondelete='CASCADE', onupdate="CASCADE"))
+    plant_id = db.Column('plant_id', db.ForeignKey('plants.plant_id', onupdate='CASCADE', ondelete="CASCADE"))
+    equipment_id = db.Column('equipment_id', db.ForeignKey('equipment.equipment_id', onupdate='CASCADE',
+                                                           ondelete="CASCADE"))
+    tag_name = db.Column('tag_name', db.Text)
+    upper_bound = db.Column('upper_bound', db.Float)
+    lower_bound = db.Column('lower_bound', db.Float)
+    units = db.Column('units', db.Text)
+
+    @staticmethod
+    def create_multiple(list):
+        for section_id, tag_name in list:
+            Tags.create(tag_name=tag_name, section_id=section_id)
+
+
+
+class Measurement_data(db.Model):
+    data_id = db.Column('data_id', db.Integer, primary_key=True)
+    timestamp = db.Column('timestamp', db.Text)
+    tag_id = db.Column('tag_id', db.ForeignKey('tags.tag_id', ondelete="CASCADE", onupdate="CASCADE"))
+    tag_value = db.Column('tag_value', db.Float)
+
+    @staticmethod
+    def write_data_to_db(file_name, plant_name, open, upload, append=False):
+
+            time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+            # adding plant name
+            Plants.create(plant_name=plant_name, opened=open, uploaded=upload, time=time)
+
+            # Finding default plant id
+            plant_id = Plants.query.with_entities(Plants.plant_id).filter_by(plant_name=plant_name).first()
+
+            # adding defualt unit name
+            default_section_name = plant_name + '_Unit01'
+            Sections.create(section_name=default_section_name, plant_id=plant_id[0])
+
+            # getting unique id for default section
+            section_id = Sections.query.with_entities(Sections.section_id).filter_by(
+                        section_name=default_section_name).first()
+
+            # handling csv file
+            df = pd.read_csv(file_name)
+            df = pd.melt(df, id_vars=df.columns.values[0])
+            df.columns = ['timestamp', 'tag_id', 'tag_value']
+            df_tags = np.unique(df['tag_id'].values)
+
+            # current tags in the database
+            # if append:
+            #     cur_tags = cur.execute('SELECT tag_name FROM tags').fetchall()
+            #
+            #     # initialize tag list to add to database
+            #     tags_commit = [(section_id, tag,) for tag in df_tags if tag not in cur_tags]
+            #
+            # else:
+                # initialize tag list to add to database
+            tags_commit = [(section_id[0], tag,) for tag in df_tags]
+
+            # adding new tags to data base
+            Tags.create_multiple(tags_commit)
+
+            # creating tagmap from tags in tags table
+            tag_map = dict(Tags.query.with_entities(Tags.tag_name, Tags.tag_id).all())
+
+
+            # normalizing dataframe tag_id with tag_ids from tags table
+            df.tag_id = [tag_map[key] for key in df['tag_id'].values]
+
+            # adding data to tag_data table
+            df.to_sql('tag_data', db.engine, if_exists='append', index=False)
