0fbd28f7b981ac9e849892ddfcbd1f8eb8417e3f
Neill Herbst <n.herbst6@gmail.com>
2017-03-07 14:14:49 +0200

    changed model parameter names to make use of classmethods easier

--- app/main/models.py
+++ app/main/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy, event
 from sqlalchemy.engine import Engine
 from sqlite3 import Connection as SQLite3Connection
+from sqlite3 import IntegrityError
 
 db = SQLAlchemy()
 
@@ -25,6 +26,7 @@ class BaseMixin:
         db.session.add(obj)
         db.session.commit()
 
+
     @classmethod
     def delete(cls, **kwargs):  # method for deleting data from table
         cls.query.filter_by(**kwargs).delete()
@@ -35,11 +37,17 @@ class BaseMixin:
     def query_columns_all(cls, *args):
         return cls.query.with_entities(*args).all()
 
+    @classmethod
+    def get_names(cls):
+        tags = cls.query.with_entities(cls.id, cls.name).all()
+        return [(str(_id), _name) for _id, _name in tags]
+
+
 
 # Model for plants table
 class Plants(BaseMixin, db.Model):
-    plant_id = db.Column('plant_id', db.Integer, primary_key=True)
-    plant_name = db.Column('plant_name', db.VARCHAR(50), unique=True)
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('plant_name', db.VARCHAR(50), unique=True)
     opened = db.Column('opened', db.Integer)
     uploaded = db.Column('uploaded', db.Integer)
     time = db.Column('time', db.TEXT)
@@ -47,27 +55,27 @@ class Plants(BaseMixin, db.Model):
 
 # Model for sections table
 class Sections(BaseMixin, db.Model):
-    section_id = db.Column('section_id', db.Integer, primary_key=True)
-    section_name = db.Column('section_name', db.TEXT)
-    plant_id = db.Column('plant_id', db.Integer, db.ForeignKey('plants.plant_id', ondelete='CASCADE',
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.TEXT)
+    plant = db.Column('plant', db.Integer, db.ForeignKey('plants.id', ondelete='CASCADE',
                                                                onupdate='CASCADE'))
 
 
 # Model for equipment table
 class Equipment(BaseMixin, db.Model):
-    equipment_id = db.Column('equipment_id', db.Integer, primary_key=True)
-    equipment_name = db.Column('equipment_name', db.Text)
-    plant_id = db.Column('plant_id', db.ForeignKey('plants.plant_id', onupdate='CASCADE', ondelete="CASCADE"))
-    section_id = db.Column('section_id', db.ForeignKey('sections.section_id', onupdate='CASCADE', ondelete="CASCADE"))
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.Text)
+    plant = db.Column('plant', db.ForeignKey('plants.id', onupdate='CASCADE', ondelete="CASCADE"))
+    section = db.Column('section', db.ForeignKey('sections.id', onupdate='CASCADE', ondelete="CASCADE"))
 
 
 # Model for tags table
 class Tags(BaseMixin, db.Model):
-    tag_id = db.Column('tag_id', db.Integer, primary_key=True)
-    section_id = db.Column('section_id', db.ForeignKey('sections.section_id', ondelete='CASCADE', onupdate="CASCADE"))
-    equipment_id = db.Column('equipment_id', db.ForeignKey('equipment.equipment_id', onupdate='CASCADE',
+    id = db.Column('id', db.Integer, primary_key=True)
+    section = db.Column('section', db.ForeignKey('sections.id', ondelete='CASCADE', onupdate="CASCADE"))
+    equipment = db.Column('equipment', db.ForeignKey('equipment.id', onupdate='CASCADE',
                                                            ondelete="CASCADE"))
-    tag_name = db.Column('tag_name', db.Text)
+    name = db.Column('name', db.Text)
     upper_bound = db.Column('upper_bound', db.Float)
     lower_bound = db.Column('lower_bound', db.Float)
     units = db.Column('units', db.Text)
@@ -75,62 +83,59 @@ class Tags(BaseMixin, db.Model):
     @staticmethod
     def create_multiple(list):
         for section_id, tag_name in list:
-            Tags.create(tag_name=tag_name, section_id=section_id)
-
-
-    @classmethod
-    def get_tags(cls):
-        tags = cls.query.with_entities(cls.tag_id, cls.tag_name).all()
-        return [(str(tag_id), tag_name) for tag_id, tag_name in tags]
-
+            Tags.create(name=tag_name, section=section_id)
 
 
 # Model for measurement data table
 class MeasurementData(db.Model):
-    data_id = db.Column('data_id', db.Integer, primary_key=True)
+    data_id = db.Column('id', db.Integer, primary_key=True)
     timestamp = db.Column('timestamp', db.Text)
-    tag_id = db.Column('tag_id', db.ForeignKey('tags.tag_id', ondelete="CASCADE", onupdate="CASCADE"))
+    tag = db.Column('tag', db.ForeignKey('tags.id', ondelete="CASCADE", onupdate="CASCADE"))
     tag_value = db.Column('tag_value', db.Float)
 
     @staticmethod
     def write_data_to_db(file_name, plant_name, open, upload):
+            try:
+                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
-            time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                # adding plant name
+                Plants.create(name=plant_name, opened=open, uploaded=upload, time=time)
 
-            # adding plant name
-            Plants.create(plant_name=plant_name, opened=open, uploaded=upload, time=time)
+                # Finding default plant id
+                plant_id = Plants.query.with_entities(Plants.id).filter_by(name=plant_name).first()
 
-            # Finding default plant id
-            plant_id = Plants.query.with_entities(Plants.plant_id).filter_by(plant_name=plant_name).first()
+                # adding defualt unit name
+                default_section_name = plant_name + '_Unit01'
+                Sections.create(name=default_section_name, plant=plant_id[0])
 
-            # adding defualt unit name
-            default_section_name = plant_name + '_Unit01'
-            Sections.create(section_name=default_section_name, plant_id=plant_id[0])
+                # getting unique id for default section
+                section_id = Sections.query.with_entities(Sections.id).filter_by(name=default_section_name).first()
 
-            # getting unique id for default section
-            section_id = Sections.query.with_entities(Sections.section_id).filter_by(
-                        section_name=default_section_name).first()
+                # handling csv file
+                df = pd.read_csv(file_name)
+                df = pd.melt(df, id_vars=df.columns.values[0])
+                df.columns = ['timestamp', 'tag', 'tag_value']
+                df_tags = np.unique(df['tag'].values)
 
-            # handling csv file
-            df = pd.read_csv(file_name)
-            df = pd.melt(df, id_vars=df.columns.values[0])
-            df.columns = ['timestamp', 'tag_id', 'tag_value']
-            df_tags = np.unique(df['tag_id'].values)
+                tags_commit = [(section_id[0], tag,) for tag in df_tags]
 
-            tags_commit = [(section_id[0], tag,) for tag in df_tags]
+                # adding new tags to data base
+                Tags.create_multiple(tags_commit)
 
-            # adding new tags to data base
-            Tags.create_multiple(tags_commit)
-
-            # creating tagmap from tags in tags table
-            tag_map = dict(Tags.query.with_entities(Tags.tag_name, Tags.tag_id).all())
+                # creating tagmap from tags in tags table
+                tag_map = dict(Tags.query.with_entities(Tags.name, Tags.id).all())
 
 
-            # normalizing dataframe tag_id with tag_ids from tags table
-            df.tag_id = [tag_map[key] for key in df['tag_id'].values]
+                # normalizing dataframe tag_id with tag_ids from tags table
+                df.tag = [tag_map[key] for key in df['tag'].values]
 
-            # adding data to tag_data table
-            df.to_sql('measurement_data', db.engine, if_exists='append', index=False)
+                # adding data to tag_data table
+                # df.to_sql('measurement_data', db.engine, if_exists='append', index=False)
+
+            except IntegrityError or KeyError as e:
+                # TODO: Add method of letting user know the write has failed
+                print(e)
+                Plants.delete(name=plant_name)
 
     @staticmethod
     def get_tag_data(**kwargs):
