1fe6aee29d52dd75e9ae34ef6d3fbcdd6aaa5cb1
NeillHerbst <n.herbst6@gmail.com>
2017-03-30 18:06:37 +0200

    removed reference to local models.py file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
 
 # imported at bottom to prevent circular importing
-from . import views, views_async, models
+from . import views, views_async
