cc24a4df6515d4024f81e5e00078b5737ee3108b
ShahuN <shaun.michael107@gmail.com>
2017-04-23 22:52:56 +0200

    Created copies of savgol filter functions in relevant plugin added run_plugin function to __init__.py

--- app/plugins/Savgol/__init__.py
+++ app/plugins/Savgol/__init__.py
@@ -1,10 +1,15 @@
 from flask import Blueprint
 from manage import AppPlugin
-from .savgol import *
+from .savgol import sg_filter
 
 __plugin__ = "SavgolFilter"
 
 savgol = Blueprint('savgol', __name__)
+
+
+def run_plugin(data_before, settings):
+    data_after = sg_filter(data_before, settings)
+    return data_after
 
 
 class SavgolFilter(AppPlugin):
--- app/plugins/Savgol/savgol.py
+++ app/plugins/Savgol/savgol.py
@@ -1,1 +1,40 @@
 from scipy.signal import savgol_filter
+import pandas as pd
+
+# TODO: Add scipy to plugin requirements
+
+
+def sg_filter(dataframe, settings):
+    dataframe_filtered, headers = _filter_df_(dataframe, settings)
+    return dataframe_filtered
+
+
+def _filter_df_(_dataframe, settings):
+    settings_1, settings_2 = settings
+    __headers = list(_dataframe)
+    arr_new__, _headers = _remove_constants_(_dataframe, __headers)
+    arr_new_ = savgol_filter(arr_new__, settings_1, settings_2, axis=0)
+    dataframe_new = _arr_to_df_(arr_new_, _headers)
+    return dataframe_new, _headers
+
+
+def _remove_constants_(_dataframe, _headers):
+    for i, h in enumerate(_headers):
+        _lst = _dataframe[h].as_matrix()
+        if _check_constant_(_lst):
+            del _dataframe[h]
+            del _headers[i]
+    return _dataframe, _headers
+
+
+def _arr_to_df_(_arr, _headings):
+    dfdict = {}
+    for i, h in enumerate(_headings):
+        dfdict[h] = _arr[:, i]
+    # dfdict = dict(zip(headings, arr)) found this method, but having issues slicing arr in one liner
+    DF = pd.DataFrame(dfdict)
+    return DF
+
+
+def _check_constant_(_lst):
+    return all(round(x, 6) == round(_lst[0], 6) for x in _lst)
