9371b68f10a77b823db09868aad146fd382b9276
Neill Herbst <n.herbst6@gmail.com>
2017-04-29 14:46:26 +0200

    removed AppPlugin Class

--- manage.py
+++ manage.py
@@ -4,16 +4,8 @@ from flask_plugins import Plugin
 from flask_uploads import UploadSet, configure_uploads, ALL
 import os
 
-
-# class for registering plugins
-class AppPlugin(Plugin):
-    def register_blueprint(self, blueprint, **kwargs):
-        """Registers a blueprint."""
-        current_app.register_blueprint(blueprint, **kwargs)
-
 # Creating app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
 
 # File upload configuration for ZIP files using flask-uploads
 plugin_upload = UploadSet('plugin', ALL)
