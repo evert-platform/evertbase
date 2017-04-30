80cdf67c1b9b6c1d0bb51970e3a7ec677abd5832
NeillHerbst <n.herbst6@gmail.com>
2017-04-30 16:53:28 +0200

    moved websocket code to evert core

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+import evertcore.websockets
 
 # main application blueprint
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
 
 # imported at bottom to prevent circular importing
-from . import views, events
+from . import views
