3040f4f4e856520ac4971d6368470ac65e80e48c
ShahuN <shaun.michael107@gmail.com>
2017-05-05 12:55:35 +0200

    updated savgol plugin to use configparser

--- app/plugins/Savgol/__init__.py
+++ app/plugins/Savgol/__init__.py
@@ -2,12 +2,21 @@ from flask import Blueprint
 from .savgol import sg_filter
 from evertcore.plugins import connect_listener, AppPlugin
 import pandas as pd
+import configparser
 __plugin__ = "SavgolFilter"
 
 savgol = Blueprint('savgol', __name__)
 
 
-def run_plugin(data_before, settings):
+def get_config():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    settings = [float(value) for key, value in config['DEFAULT'].items()]
+    return settings
+
+
+def run_plugin(data_before):
+    settings = get_config()
     if isinstance(data_before, pd.DataFrame) and isinstance(settings, list):
         data_after = sg_filter(data_before, settings)
     else:
@@ -19,5 +28,4 @@ class SavgolFilter(AppPlugin):
 
     def setup(self):
         self.register_blueprint(savgol)
-        # There should be a connect event here, I'm unsure how Neill wants this.
-        # connect_listener('data_upload', run_plugin)
+        connect_listener('data_upload', run_plugin)
