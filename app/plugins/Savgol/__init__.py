4f8856a3c4d8fc8a50b8b5003ba4bcc1ba56288e
ShahuN <shaun.michael107@gmail.com>
2017-05-05 12:39:52 +0200

    Added type check for savgol filter plugin

--- app/plugins/Savgol/__init__.py
+++ app/plugins/Savgol/__init__.py
@@ -1,14 +1,17 @@
 from flask import Blueprint
 from .savgol import sg_filter
 from evertcore.plugins import connect_listener, AppPlugin
-
+import pandas as pd
 __plugin__ = "SavgolFilter"
 
 savgol = Blueprint('savgol', __name__)
 
 
 def run_plugin(data_before, settings):
-    data_after = sg_filter(data_before, settings)
+    if isinstance(data_before, pd.DataFrame) and isinstance(settings, list):
+        data_after = sg_filter(data_before, settings)
+    else:
+        raise ValueError("Incorrect input types for Savgol Filter")
     return data_after
 
 
