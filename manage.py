0903d7032c9679230d5e65b3217bf991ff73492a
NeillHerbst <n.herbst6@gmail.com>
2017-05-09 17:54:33 +0200

    running socketio

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from app import create_app, socketio
 from flask_uploads import UploadSet, configure_uploads, ALL
 import os
+import eventlet
+eventlet.monkey_patch()
 
 # Creating app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -10,4 +12,4 @@ plugin_upload = UploadSet('plugin', ALL)
 configure_uploads(app, plugin_upload)
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
