022b4cdb6516155ef668a31d55a2bc2337d19688
Neill Herbst <n.herbst6@gmail.com>
2017-05-10 11:43:29 +0200

    implemented new plugins (cannot read config files in multiprocessing process)

--- app/plugins/features/__init__.py
+++ app/plugins/features/__init__.py
@@ -4,6 +4,8 @@ from evertcore.plugins import connect_listener, AppPlugin
 import time
 import pandas as pd
 import configparser
+from evertcore.websockets import socketio
+import os
 
 __plugin__ = "FeatureExtraction"
 
@@ -14,18 +16,21 @@ def get_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
     settings = [float(value) for key, value in config['DEFAULT'].items()]
+    socketio.emit('connected', {'msg': settings}, namespace='/test')
     return settings
 
 
 def run_plugin(data_before, **kwargs):
-    # q = kwargs['q']
-    # data_after = extract_features(data_before, settings)
-    # q.put(data_after)
-    settings = get_config()
+    print('event_emitted')
+    a = os.path.curdir
+    settings = [0.3, 3, 50]
+    socketio.emit('connected', {'msg': a}, namespace='/test')
     if isinstance(data_before, pd.DataFrame) and isinstance(settings, list):
         data_after = extract_features(data_before, settings)
     else:
         raise ValueError("Incorrect input types for Savgol Filter")
+
+    socketio.emit('connected', {'msg': data_after}, namespace='/test')
     return data_after
 
 
@@ -33,4 +38,4 @@ class FeatureExtraction(AppPlugin):
 
     def setup(self):
         self.register_blueprint(features)
-        connect_listener("data_upload", run_plugin)
+        connect_listener("zoom_event", run_plugin)
