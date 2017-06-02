508cfba6c035e961544da907a74a534db4ac6b78
Neill Herbst <n.herbst6@gmail.com>
2017-06-02 13:05:33 +0200

    added new socket method

--- evertcore/websockets.py
+++ evertcore/websockets.py
@@ -1,13 +1,24 @@
 from flask_socketio import emit, SocketIO
 
-socketio = SocketIO(message_queue='amqp://guest:guest@localhost:5672//')
+socketio_mp = SocketIO(message_queue='amqp://guest:guest@localhost:5672//')
+print(socketio_mp)
 
 
-@socketio.on('connect', namespace='/test')
+
+@socketio_mp.on('connected', namespace='/test')
 def joined():
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
     print('connected')
     emit('connected', {'msg': 'hello'})
 
+
+@socketio_mp.on('zoomed', namespace='/test')
+def zoom_event(data):
+
+
+    print('zoom')
+    print(data)
+    emit('zoom_return', {'msg': 'zoom_return'})
+    return
 
