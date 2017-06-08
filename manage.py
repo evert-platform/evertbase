360f2c22510201fecbbe60ae5663109737aecbae
Neill Herbst <n.herbst6@gmail.com>
2017-06-08 14:54:49 +0200

    moved socket init to manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
-from app import create_app, socketio
+from app import create_app
 from flask_uploads import UploadSet, configure_uploads, ALL
 import os
 import eventlet
+from evertcore.websockets import socketio
 eventlet.monkey_patch()
 
 # Creating app
@@ -10,6 +11,8 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 # File upload configuration for ZIP files using flask-uploads
 plugin_upload = UploadSet('plugin', ALL)
 configure_uploads(app, plugin_upload)
+
+socketio.init_app(app, message_queue=app.config['MESSAGE_QUEUE'])
 
 if __name__ == '__main__':
     socketio.run(app)
