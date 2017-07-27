b700a96a1c8f21dd3f46e5bd00ef6bea6df55feb
Neill Herbst <n.herbst6@gmail.com>
2017-07-27 13:55:44 +0200

    successful data reading of zoom event websocket data

--- evertcore/websockets.py
+++ evertcore/websockets.py
@@ -19,18 +19,29 @@ def joined():
 @socketio.on('zoom_event', namespace='/test')
 def zoom_event(socket_data):
     print('socket zoom event')
+    print(socket_data)
+    print()
+    try:
+        tmin = datetime.strptime(socket_data['domain'][0], '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        tmin = datetime.strptime(socket_data['domain'][0], '%Y-%m-%d %H:%M:%S')  # Does not contain milliseconds
 
-    domain = socket_data['domain']
+    try:
+        tmax = datetime.strptime(socket_data['domain'][1], '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        tmax = datetime.strptime(socket_data['domain'][1], '%Y-%m-%d %H:%M:%S')  # Does not contain milliseconds
+
     tags = socket_data['ids']
-    domain = [float(d) / 1000 for d in domain]
 
-    tag_data_ = data.tag_data(tags, datetime.fromtimestamp(domain[0]), datetime.fromtimestamp(domain[1]))
-    fig = plotting.Fig()
-    fig.prepare_data(tag_data_, threshold=_threshold)
-    data_, datamap = fig.return_data()
-    window_data = fig.window_data(domain)
-    plugins.emit_event('zoom_event', window_data, fig.domain)
 
-    emit('zoom_return', dict(success=True, data=data_, datamap=datamap, domain=domain))
+    #
+    # tag_data_ = data.tag_data(tags, datetime.fromtimestamp(domain[0]), datetime.fromtimestamp(domain[1]))
+    # fig = plotting.Fig()
+    # fig.prepare_data(tag_data_, threshold=_threshold)
+    # data_, datamap = fig.return_data()
+    # window_data = fig.window_data(domain)
+    # plugins.emit_event('zoom_event', window_data, fig.domain)
+    #
+    # emit('zoom_return', dict(success=True, data=data_, datamap=datamap, domain=domain))
     return
 
