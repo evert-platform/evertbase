7639de607aa5848d18ca4db7924757af1fb1869f
NeillHerbst <n.herbst6@gmail.com>
2017-07-31 17:57:47 +0200

    scattter and line graphs now set up properly

--- app/plugins/features/tsfresh_mini.py
+++ app/plugins/features/tsfresh_mini.py
@@ -124,7 +124,7 @@ def _format_data_(header_name, feature_name, feature_timestamp, feature_value, *
             'name': feature_name,
             'x': [feature_timestamp[0], feature_timestamp[1]],
             'y': [feature_value, feature_value],
-            'type': 'line'
+            'mode': 'lines'
         }
 
     else:
@@ -133,7 +133,7 @@ def _format_data_(header_name, feature_name, feature_timestamp, feature_value, *
             'name': feature_name,
             'x': [feature_timestamp],
             'y': [feature_value],
-            'type': 'scatter'
+            'mode': 'markers'
 
         }
 
