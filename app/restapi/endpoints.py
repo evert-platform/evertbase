5e9b02b7ab4f7e1cb26959bf6f7bf6251fed2e3b
Neill Herbst <n.herbst6@gmail.com>
2017-08-18 12:25:23 +0200

    added variables for linked x axes

--- app/restapi/endpoints.py
+++ app/restapi/endpoints.py
@@ -14,8 +14,10 @@ def _plotdata():
     tags = request.args.getlist('tags[]')
     subplots = request.args.get('subplotCheck', 'false', type=str)
     subplots = False if subplots == 'false' else True
+    linkx = request.args.get('linkXaxes', 'false', type=str)
+    linkx = False if linkx == 'false' else True
     tag_data = evert.data.tag_data(tags)
-    fig = evert.plotting.Fig(subplots=subplots)
+    fig = evert.plotting.Fig(subplots=subplots, link_xaxes=linkx)
     fig.prepare_data(tag_data, _threshold)
     data, datamap = fig.return_data()
     # evert.plugins.emit_event('zoom_event', fig.dataFrame, None)
