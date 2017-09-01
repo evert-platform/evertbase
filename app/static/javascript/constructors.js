7deef12ecfa665595ec0bf2c4766cb7056660662
NeillHerbst <n.herbst6@gmail.com>
2017-09-01 16:21:39 +0200

    added read and write state messages

--- app/static/javascript/constructors.js
+++ app/static/javascript/constructors.js
@@ -26,4 +26,22 @@ function EvertPlotState(){
         this.formData = {};
     };
 
+    this.readState = function(state){
+        this.traces = state.traces;
+        this.subplots = state.subplots;
+        this.linkedXAxis = state.linkedXAxis;
+        this.plotLayout = state.plotLayout;
+        this.formData = state.formData;
+    };
+
+    this.writeState = function() {
+        return {
+            traces: this.traces,
+            subplots: this.subplots,
+            linkedXAxis: this.linkedXAxis,
+            plotLayout: this.plotLayout,
+            formData: this.formData
+        };
+    }
+
 }
