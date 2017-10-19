bec6d2ff9220ff85fc9c6dd77320662e701c446c
NeillHerbst <n.herbst6@gmail.com>
2017-10-19 18:34:09 +0200

    moved socket zoom event to seperate folder

---
+++ app/static/javascript/socketevents.js
@@ -0,0 +1,48 @@
+function link_zoom_event(socket, DOMStrings, plotStateObject) {
+    var plotArea = document.getElementById("plot");
+    plotArea.on("plotly_relayout", function (e) {
+        var keys = Object.keys(e);
+        var names;
+
+        if (keys.length > 0 && keys[0].match(/(xaxis[0-9]*)(?=\.range\[[0-9]\])/g) &&
+            keys[1].match(/(xaxis[0-9]*)(?=\.range\[[0-9]\])/g)) {
+
+            var xmin = e[keys[0]];
+            var xmax = e[keys[1]];
+
+            if (!$(DOMStrings.subplotsCheck).is(":checked")) {
+                socket.emit("zoom_event",
+                    {
+                        domain: [xmin, xmax],
+                        ids: $(DOMStrings.tags).val()
+                    });
+            } else {
+                var xAxis = keys[0].match(/(xaxis[0-9]*)(?=\.range\[[0-9]\])/g)[0];
+                var xAxisNumber = xAxis.match(/([0-9])/g);
+                if (!xAxisNumber) {
+                    names = _.partition(plotArea.data, function (d) {
+                        return _.includes(["x"], d.xaxis);
+                    })[0];
+
+                } else if (xAxisNumber) {
+                    names = _.partition(plotArea.data, function (d) {
+                        return _.includes(["x".concat(xAxisNumber)], d.xaxis);
+                    })[0];
+                }
+
+                var ids = [];
+                names.forEach(function (d, i) {
+                    ids.push(plotStateObject.tagsMap[d.name]);
+                });
+                socket.emit("zoom_event",
+                    {
+                        domain: [xmin, xmax],
+                        ids: ids,
+                        xAxisNo: xAxisNumber || [1]
+                    });
+            }
+        }
+
+        plotStateObject.plotLayout = plotArea.layout;
+    });
+}
