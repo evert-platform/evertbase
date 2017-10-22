457ea939f8e9d0722edd1af4adb0cdca05fc27db
NeillHerbst <n.herbst6@gmail.com>
2017-10-22 12:16:33 +0200

    split data update and plugin update socket

--- evertcore/websockets.py
+++ evertcore/websockets.py
@@ -1,7 +1,6 @@
 from flask_socketio import emit, SocketIO
 from datetime import datetime
 
-
 from . import data, plotting, plugins
 
 socketio = SocketIO()
@@ -16,6 +15,7 @@ def joined():
 
 @socketio.on('zoom_event', namespace='/test')
 def zoom_event(socket_data):
+    print(socket_data)
 
     try:
         tmin = datetime.strptime(socket_data['domain'][0], '%Y-%m-%d %H:%M:%S.%f')
@@ -30,14 +30,33 @@ def zoom_event(socket_data):
     tags = socket_data['ids']
 
     tag_data_ = data.tag_data(tags, tmin, tmax)
-
     fig = plotting.Fig()
     fig.prepare_data(tag_data_, threshold=_threshold)
     data_ = fig.return_data()
     emit('zoom_return', dict(success=True, data=data_))
 
-    window_data = fig.window_data(socket_data['domain'])
-    plugins.emit_event('zoom_event', window_data, fig.domain, socket_data['axisMap'])
+    return
+
+
+@socketio.on('update_plugins_event', namespace='/test')
+def update_plugin_data(socket_data):
+    try:
+        tmin = datetime.strptime(socket_data['domain'][0], '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        tmin = datetime.strptime(socket_data['domain'][0], '%Y-%m-%d %H:%M:%S')  # Does not contain milliseconds
+
+    try:
+        tmax = datetime.strptime(socket_data['domain'][1], '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        tmax = datetime.strptime(socket_data['domain'][1], '%Y-%m-%d %H:%M:%S')  # Does not contain milliseconds
+
+    tags = socket_data['ids']
+
+    tag_data_ = data.tag_data(tags, tmin, tmax)
+    fig = plotting.Fig()
+    fig.prepare_data(tag_data_, threshold=_threshold)
+    data_ = fig.window_data(socket_data['domain'])
+    plugins.emit_event('zoom_event', data_, fig.domain, socket_data['axisMap'])
     return
 
 
