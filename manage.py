66ed54a4bfff9d02fe1fae78bc9b86274c0abf08
NeillHerbst <n.herbst6@gmail.com>
2017-12-06 18:10:41 +0200

    removed unused code

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 from app import create_app
-from flask_uploads import UploadSet, configure_uploads, ALL
 import os
 import eventlet
 from evertcore.websockets import socketio
@@ -7,11 +6,6 @@ eventlet.monkey_patch()
 
 # Creating app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-# File upload configuration for ZIP files using flask-uploads
-plugin_upload = UploadSet('plugin', ALL)
-configure_uploads(app, plugin_upload)
-
 socketio.init_app(app, message_queue=app.config['MESSAGE_QUEUE'])
 
 if __name__ == '__main__':
